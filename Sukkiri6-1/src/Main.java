d078cf25d88232fb65000ca9d388123b014db7d2
edm20627 <edm2.0627@gmail.com>
2018-12-04 14:18:36 +0900

    12/04

---
+++ Sukkiri6-1/src/Main.java
@@ -0,0 +1,10 @@
+import comment.Zenhan;
+
+public class Main {
+	public static void main(String[] argas)throws Exception{
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.kouhan.callDeae();
+		comment.kouhan.showMondokoro();
+	}
+}
